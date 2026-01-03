b7f33aed761b0176825412f4a85d79bdb389a4d7
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-01-03 15:58:08 -0800

    Initial Commit for receiving video input
    Add Video Data module for CNN input preprocessing:
    TODO: handle receiving video input from the frontend, cleaning the video frames, and formatting them into a structure suitable for CNN models.

---
+++ backend/VideoData.py
@@ -0,0 +1,8 @@
+"""
+NP ˚❀༉‧ Preprocessing video data for CNN input.
+This module handles receiving video input from the frontend, cleaning the video frames, and formatting them into a structure suitable for CNN models.
+"""
+# ~~~~ Necessary Imports ~~~~
+import cv2  # OpenCV (Open Source Computer Vision Library)
+import numpy as np
+from typing import Optional
