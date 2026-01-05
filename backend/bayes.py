33ef5f60b50c2a52f909045f3cbf6b0f462a0b98
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-01-05 13:31:19 -0800

    Initial Commit ‧˚❀༉‧˚. Bayes
    Initial commit to begin implementation of a Bayes Decision Tree for classification.

---
+++ backend/bayes.py
@@ -0,0 +1,8 @@
+""" NP ‧˚❀༉‧˚. Bayes Decision Tree
+Loads & Fit the Data into a Decision tree
+Builds to the input data so that the tree can be populated
+Classification Method (the goal is to predict discrete labels for potability)
+"""
+#~~~~ Necessary Imports
+import numpy as np
+from collections import Counter
