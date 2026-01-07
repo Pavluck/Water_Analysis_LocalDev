173f3bd0825892e19e282b92d26925e7c12cf42b
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-01-07 14:14:04 -0800

    Begin writing global functions ✧
    Added a function to load CSV files into a DataFrame.

--- backend/datacleaning.py
+++ backend/datacleaning.py
@@ -2,3 +2,16 @@
 """
 ˚❀༉‧ Clean and preprocess water analysis data
 """
+# ~~~~ Imports
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# ~~~~ Global Functions
+def load(filepath):
+  """Given a filepath to a CSV file, returns the dataframe"""
+  df = pd.read_csv(filepath)
+  return df
+  
