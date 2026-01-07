951e4f356b34ad79bb287f440fdfb288afb60153
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-01-06 17:44:11 -0800

    entropy function ✧
    Added a global function to calculate entropy for uncertainty measurement based on feature occurrences.

--- backend/bayes.py
+++ backend/bayes.py
@@ -6,3 +6,10 @@ Classification Method (the goal is to predict discrete labels for potability)
 #~~~~ Necessary Imports
 import numpy as np
 from collections import Counter
+#~~~~ Global Functions
+def entropy(features):
+    """This method calculates the measure of uncertainty"""
+    histogram = np.bincount(features) # calculates number of occurrences of class labels
+    prob = histogram / len(features)  # histogram 
+    uncertainty = -np.sum([p * np.log2(p) for p in prob if p > 0])
+    return uncertainty
