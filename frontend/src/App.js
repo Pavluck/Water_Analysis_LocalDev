306148e5bb2a04ad2f0f25e8721876899a8e671d
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-01-09 12:22:55 -0800

    init for frontend runscript

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// ------- Necessary Imports -----------
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+// ------- Page Content -----------
+function App() {
+  return (
+      <div className='App'>
+      <footer>
+            <p> &copy; 2026 Natasha Pavelek</p>
+      </footer>
+      </div>
+  );
+}
+export default App;
