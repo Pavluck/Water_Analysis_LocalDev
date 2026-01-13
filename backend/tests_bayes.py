01b31871ea1aacb9fe7e17f5d30c1aebe3194670
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-01-12 20:47:38 -0800

    ★ accuracy global function for model evaluation
    Add accuracy function to evaluate predictions

--- backend/tests_bayes.py
+++ backend/tests_bayes.py
@@ -5,3 +5,9 @@ import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from bayes import DecisionTree
+
+# ~~~~ Global Function(s) 
+def accuracy(true, predicted):
+  """Given the true and predicted features, compare accuracy"""
+  accuracy = np.sum(true == predicted)/len(true)
+  return accuracy
