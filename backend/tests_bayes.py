9b365ac22450170d67bca0c6a1221d11acb1266d
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-01-12 20:45:28 -0800

    Init bayes decision tree accuracy/tests ★
    Started file for tests for bayes decision tree to compare accuracy with scikit-learn.

---
+++ backend/tests_bayes.py
@@ -0,0 +1,7 @@
+# ~~~~ NP: tests for bayes decision tree, compare accuracy with scikit learn
+
+# -------------Necessary Imports-------------
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from bayes import DecisionTree
