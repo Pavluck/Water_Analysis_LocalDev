d477beb899959cb3df5ae2f27f8a8f47a88e0a78
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-01-12 20:56:28 -0800

    ★ decision tree tests and accuracy
    Added a decision tree classifier to test accuracy on the breast cancer dataset.

--- backend/tests_bayes.py
+++ backend/tests_bayes.py
@@ -11,3 +11,21 @@ def accuracy(true, predicted):
   """Given the true and predicted features, compare accuracy"""
   accuracy = np.sum(true == predicted)/len(true)
   return accuracy
+
+# ------------- Load & Test -------------
+data = datasets.load_breast_cancer()
+Samples = data.data
+features = data.target
+
+Samples_train, Samples_test, feat_train, feat_test = train_test_split(Samples, features, test_size=0.2, random_state=1234)
+
+clf = DecisionTree(max_depth=10)
+# create decision with built tree
+clf.fit(Samples_train, feat_train)
+
+feat_prediction = clf.predict(Samples_test)
+acc = accuracy(feat_test, feat_prediction)
+
+print("Accuracy: ", acc)
+# turns out to be about ~ 6052631578947368
+# scikit learn's is better, closer to 0.95
