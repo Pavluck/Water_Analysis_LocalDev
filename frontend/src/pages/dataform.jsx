ac27f54c1fbf3144fb00ac9ed5ff9a900014aa05
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-01-16 18:26:08 -0800

    Init for dataform.jsx
    Start a dataform page for the frontend

---
+++ frontend/src/pages/dataform.jsx
@@ -0,0 +1,16 @@
+/* ------------ Page to allow the user to give data about a body of water ------------*/
+
+/* ------------ Necessary Imports ------------*/
+import React, { useState, useRef} from 'react';
+import { CirclePicker } from 'react-color';
+import axios from 'axios';
+import { CgDrop } from "react-icons/cg";
+import { useNavigate } from 'react-router-dom';
+import References from './references';
+import Home from './home';
+
+/* ------------ Navigation ------------*/
+function WaterAIform() {
+  return ()
+}
+export default WaterAIform;
