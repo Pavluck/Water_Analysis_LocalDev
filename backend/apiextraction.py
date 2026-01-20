c08607ba92efc5eb499c0a944e1fa9c78f469d37
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-01-19 17:40:44 -0800

    Init apiextraction.py
    image dataset retrieval: retrieves and organizes an image dataset using an API key from Figshare.

---
+++ backend/apiextraction.py
@@ -0,0 +1,10 @@
+"""
+NP ˚❀༉‧ 
+Retrieve and organize image dataset using an API key.
+Does this currently with one API key, from Figshare:
+https://figshare.com/articles/dataset/A_Comprehensive_Surface_Water_Quality_Monitoring_Dataset_1940-2023_2_82Million_Record_Resource_for_Empirical_and_ML-Based_Research/27800394/2
+"""
+# ~~~~ Imports ~~~~
+import requests
+import os
+from tqdm import tqdm # cute task bar 
