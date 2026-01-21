84406304ebbae0642ad5e86ee533287750855013
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-01-20 23:42:35 -0800

    App.js init with imports

---
+++ frontend/App.js
@@ -0,0 +1,10 @@
+// ------- Natasha Pavelek 2026 -----------
+
+// ------- Necessary Imports -----------
+import './tealwater.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from "./pages/home";
+import WaterAIform from "./pages/dataform";
+import References from "./pages/references";
+
+// ------- Page Content -----------
