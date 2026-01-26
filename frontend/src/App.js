4d1cd42939c065f11e96f840fe3c1e4824d2225c
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-01-25 18:53:10 -0800

    ☆ structure and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-// ------- Necessary Imports -----------
+// ------- Imports -----------
+import './tealwater.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from "./pages/home";
+import WaterAIform from "./pages/dataform";
+import References from "./pages/references";
 
-// ------- Page Content -----------
+// ~~~~~~ Page Content ~~~~~~
 function App() {
   return (
       <div className='App'>
+        <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/dataform" element={<WaterAIform />} />
+          <Route path="/references" element={<References />} />
+        </Routes>
+        </Router>
       <footer>
             <p> &copy; 2026 Natasha Pavelek</p>
       </footer>
