ea1d78a1cee4b3bff6e972a7a337676103867c58
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-02-05 21:20:16 -0800

    Update test_videodata.py

--- backend/test_videodata.py
+++ backend/test_videodata.py
@@ -12,3 +12,11 @@ from videodata import VideoProcessor
 import cv2
 import numpy as np
 import subprocess
+
+# Test with live stream video URL, make sure it can recieve a livestream
+test_url = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+processedvid = VideoProcessor(target_size=(224, 224), fps=30)
+frames = processedvid.process_video(test_url)
+print("Testing... Processed frames shape:", frames.shape if frames is not None else "Test failed: No frames processed"
+# Test should succeed with output: 
+# Testing...Processed frames shape: (14315, 224, 224, 3)
