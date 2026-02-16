78a6e83f66fc2d2c91e2215bb700d1f69fa069b0
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-02-15 21:50:09 -0800

    ˚❀༉ Init Commit for CNN.py

---
+++ backend/CNN.py
@@ -0,0 +1,6 @@
+"""
+˚❀༉‧ CNN (Convolutional Neural Network)
+Training a CNN from a dataset of images to be able to determine potability of water from a live stream or video. 
+Feature Complexity: While the model can identify visual features like clarity, 
+it might struggle to accurately predict complex water parameters such as PHlevels or organic matter from an image alone. 
+"""
