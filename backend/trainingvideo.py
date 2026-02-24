30b36cee36c448e6c13ccda5249538504b583b99
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-02-23 21:43:01 -0800

    Refactor WaterQualityDataset class structure

--- backend/trainingvideo.py
+++ backend/trainingvideo.py
@@ -5,11 +5,8 @@ training a CNN model to classify water clarity, and managing training/validation
 """
 
 # ~~~~ Necessary Imports ~~~~
-import os
 import pandas as pd
-import numpy as np
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
 import torch
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
@@ -21,13 +18,14 @@ Testing = r"ML_Water_LocalDev\Water-Images\valid"
 Training_labels = r"ML_Water_LocalDev\Water-Images\train\_annotations.csv"
 Testing_labels = r"ML_Water_LocalDev\Water-Images\valid\_annotations.csv"
 
-class WaterQualityDataset(Dataset):
+class WaterQualityDataset:
     """reads data from CSV and returns (image_tensor, label)."""
     Potable_labels = {'clear': 'potable', 'murky': 'not_potable'}
 
-    def __init__(self, csv_file, img_dir, transform=None, label_mode='clarity'):
-        self.data = pd.read_csv(csv_file)
-        self.img_dir = img_dir
+    def __init__(self, labels, images, transform=None, label_mode=None):
+        """Anchor the labels, images, the transformation and label type"""
+        self.labels = labels
+        self.images = imgages
         self.transform = transform
         self.label_mode = label_mode
 
@@ -36,7 +34,10 @@ class WaterQualityDataset(Dataset):
 
         self.label_encoder = LabelEncoder()
         self.label_encoder.fit(self.data['class'].unique())
-    
+
+    def __len__(self):
+        """Returns the total number of samples in the dataset"""
+        return len(self.images)
 
     def __getitem__(self, index):
         """ Using an index from the dataframe, PyTorch matches it to fetch the image data and labels for a specific index during training. """
