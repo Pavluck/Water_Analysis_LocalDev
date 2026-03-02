72ba64436f171a917d36f443d14b4d42461fa4e9
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-03-01 22:06:22 -0800

    ✧ training and transforming data

--- backend/trainingvideo.py
+++ backend/trainingvideo.py
@@ -48,4 +48,14 @@ class WaterQualityDataset:
             image = self.transform(image)
         return image, label
 
-# TODO: ~~~~ Training & Transforming with the loaded data ~~~~~
+# TODO: ~~~~ Finish Training & Transforming with the loaded data ~~~~~
+train_transforms = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(10),
+    transforms.ColorJitter(0.2,0.2),
+    transforms.ToTensor(),
+    # Imagenet pretrained model uses this mean and stD:
+    transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
+    # TODO: Recalculate for water images
+])
