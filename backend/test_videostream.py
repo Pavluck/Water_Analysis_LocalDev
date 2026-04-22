fc9ca12bfb95ec7bcddbfb594fb276261a17247d
Natasha Pavelek <137955689+Pavluck@users.noreply.github.com>
2026-04-21 21:29:23 -0700

    ✧ tests for video stream and model setup
    Added tests for video stream processing and setup.

--- backend/test_videostream.py
+++ backend/test_videostream.py
@@ -1,4 +1,5 @@
 """
+✧ Natasha Pavelek
 Tests for the Video Data 
 Needs to recieve video
 Then it needs to Process/Normalize correctly
@@ -12,7 +13,15 @@ from videodata import VideoProcessor
 import cv2
 import numpy as np
 import subprocess
+import csv
+import tempfile
+from pathlib import Path
+import torch
+from torchvision import transforms
+import requests
+import yt_dlp
 
+# ~~~ Test 0 ~~~
 # Test with live stream video URL, make sure it can recieve a livestream
 test_url = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
 processedvid = VideoProcessor(target_size=(224, 224), fps=30)
@@ -20,3 +29,13 @@ frames = processedvid.process_video(test_url)
 print("Testing... Processed frames shape:", frames.shape if frames is not None else "Test failed: No frames processed"
 # Test should succeed with output: 
 # Testing...Processed frames shape: (14315, 224, 224, 3)
+
+# ~~~ Test Setup ~~~
+Filepath = Path(__file__).parent
+CNN_Name = "water_potability_image_model.pth"  # version 2.1
+CNNPath = Filepath / CNN_Name
+CSV = "water_potability_test_results.csv"
+CSVOutput = Filepath / CSVFilename
+
+
+# ~~~ Test 1 ~~~
